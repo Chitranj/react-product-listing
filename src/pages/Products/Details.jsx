9ab922b267243de66dcc86e69ab7dad2e17cb89d
chitranjan kumar <chitranjan.kumar@msllearning.com>
2026-02-03 10:54:07 +0530

    modify product-details page

--- src/pages/Products/Details.jsx
+++ src/pages/Products/Details.jsx
@@ -2,12 +2,17 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import { API_BASE_URL } from '../../config/api';
 import { useParams, useSearchParams } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStar,faIndianRupee } from '@fortawesome/free-solid-svg-icons';
 
 const Details = () => {
 
     const params = useParams();
     
     const [productDetails, setProductDetails] = useState({});
+    const discountPercent = Math.floor(productDetails.discountPercentage);
+
+    const newPrice = productDetails.price*((100-discountPercent)/100);
 
     const getSingleProduct = async () => {
         try {
@@ -25,11 +30,46 @@ const Details = () => {
 
     return (
         <>
-            <div className='w-[96%] my-5 mx-auto'>
+            <div className='xl:w-[80%] md:w-[90%] my-5 mx-auto flex'>
                 <div className='w-1/2 bg-white border border-gray-300 rounded-xl'>
-                    <img src={productDetails.images} alt="" className='h-125 w-auto mx-auto'/>
-                    <div className='m-7'>
-                        <button className='bg-green-500 text-white px-3 py-2 rounded text-sm font-medium'>Add To Cart</button>
+                    <figure>
+                        <img src={productDetails.thumbnail} alt={productDetails.title} className='h-100 w-auto mx-auto'/>
+                    </figure>
+                    <div className=''>
+                        <div className='grid sm:grid-cols-6 m-5 gap-1 content-center justify-items-center'>
+                            {
+                                productDetails?.images?.length ? productDetails.images.map((images, index) => {
+                                    return (
+                                        <div className='border p-2 border-gray-200 rounded cursor-pointer hover:border-gray-400' key={index}>
+                                            <img src={images} alt={images} className='w-25 h-auto mx-auto'/>
+                                        </div>
+                                    )
+                                })
+                                : ''
+                            }
+                        </div>
+                    </div>
+                </div>
+                <div className='w-1/2'>
+                    <div className='ms-2 flex flex-col'>  
+                        <h2 className='text-xl font-medium text-gray-700'>{productDetails.title}</h2>
+                        <div className='flex mt-1'>
+                            <div className='text-xs font-medium bg-green-700 px-2 py-0.5 rounded text-white content-center cursor-pointer'>
+                                <span>{productDetails?.rating?.toFixed(1) ?? 0.0}</span>
+                                <FontAwesomeIcon icon={faStar} className='ms-1 text-[10px]'/>
+                            </div>
+                            <div className='text-sm font-medium text-gray-600'>
+                                <div className='ms-2'>3 Reviews</div>
+                            </div>
+                        </div>
+                        <div className='mt-1'>
+                            <FontAwesomeIcon icon={faIndianRupee} className='text-xl'/>
+                            <span className='font-medium text-xl'>{Math.ceil(newPrice)}</span>
+                            <span className='font-medium text-gray-600 ms-2 line-through text-sm'>
+                                <FontAwesomeIcon icon={faIndianRupee} className=''/>{Math.ceil(productDetails.price)}
+                            </span>
+                            <span>{Math.floor(productDetails.discountPercentage)} % Off</span>
+                        </div>
                     </div>
                 </div>
             </div>
